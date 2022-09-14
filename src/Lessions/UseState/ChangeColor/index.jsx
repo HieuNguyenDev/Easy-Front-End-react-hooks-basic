7349886f9b658789de43e1f71e54be240974ba88
Nguyen Minh Hieu <86197383+HieuNguyenDev@users.noreply.github.com>
2022-09-14 19:56:42 +0700

    update lession 2

--- src/Lessions/UseState/ChangeColor/index.jsx
+++ src/Lessions/UseState/ChangeColor/index.jsx
@@ -5,9 +5,11 @@ function ColorBox() {
   const storageColor = JSON.parse(localStorage.getItem("color"));
   const colors = ["deeppink", "green", "yellow", "black", "blue"];
   const [color, setColor] = useState(storageColor ?? "deeppink");
+
   const handleChangeColor = () => {
     const lengthColors = colors.length;
     const index = Math.floor(Math.random() * lengthColors);
+
     setColor(() => {
       const newColor = colors[index];
       const jsonColor = JSON.stringify(newColor);
@@ -21,7 +23,9 @@ function ColorBox() {
         className="box"
         style={{ backgroundColor: color }}
         onClick={handleChangeColor}
-      ></div>
+      >
+        <h1 style={{color: colors[Math.floor((Math.random() * colors.length))]}}>Color Box</h1>
+      </div>
     </div>
   );
 }
